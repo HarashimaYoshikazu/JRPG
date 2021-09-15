3313c9bde9317017b177f63443fbb2fe4c124ae9
HarashimaYoshikazu <88476369+HarashimaYoshikazu@users.noreply.github.com>
2021-09-15 13:13:42 +0900

    課題提出の準備

---
+++ Assets/Scripts/Commands/BlockCommandSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BlockCommandSO : CommandSO
+{
+    [SerializeField] int def;
+
+    public override void Execute(Unit user, Unit target)
+    {
+        //target.hp -= wisdom;
+        Debug.Log($"{user.name}は身を守った！");
+    }
+}
